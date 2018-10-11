8fcc8dee097cb770f5b257b352a8befcd0d60b06
Ayush Shrestha <43790475+IamAyushShrestha@users.noreply.github.com>
2018-10-11 00:25:00 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+// Author AYUSH SHRESTHA
+import java.util.Random;
+import java.util.Scanner;
+impport java.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    int [] myList= new [10];
+    for (int i =1;i<=10;i++){
+        myList= in.nextInt();
+    }
+    for (int i=1;i<=10;i++){
+        System.out.println(myList[i]);
+        System.out.println();
+    }
+    }
+
+}
+
